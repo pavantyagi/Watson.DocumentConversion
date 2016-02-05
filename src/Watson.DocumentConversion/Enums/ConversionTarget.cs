b966f42388d5480cafc4024710d04a45332467f2
Damien Dennehy <damien@dennehy.me>
2016-02-05 13:57:43 -0700

    Added ConversionTarget enum.

---
+++ src/Watson.DocumentConversion/Enums/ConversionTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace Watson.DocumentConversion.Enums
+{
+    /// <summary>
+    ///     The output format that you want to convert the document to.
+    /// </summary>
+    public enum ConversionTarget
+    {
+        /// <summary>
+        ///     Html
+        /// </summary>
+        [EnumMember(Value = "NORMALIZED_HTML")] Html,
+
+        /// <summary>
+        ///     Text
+        /// </summary>
+        [EnumMember(Value = "NORMALIZED_TEXT")] Text,
+
+        /// <summary>
+        ///     Answer Units in Json format.
+        /// </summary>
+        [EnumMember(Value = "ANSWER_UNITS")] AnswerUnits
+    }
+}
--- src/Watson.DocumentConversion/IDocumentConversionService.cs
+++ src/Watson.DocumentConversion/IDocumentConversionService.cs
@@ -1,5 +1,11 @@
 ﻿namespace Watson.DocumentConversion
 {
+    /// <summary>
+    ///     The Watson Document Conversion service converts a single HTML, PDF, or Microsoft Word document
+    ///     into a normalized HTML, plain text, or a set of JSON-formatted Answer units that can be used with
+    ///     other Watson services.
+    ///     <para>http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/document-conversion.html</para>
+    /// </summary>
     public interface IDocumentConversionService
     {
     }
