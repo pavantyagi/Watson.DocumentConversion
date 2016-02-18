41cb05aa43081b80f9b4c49e81818969bb4c4f23
Damien Dennehy <damien@dennehy.me>
2016-02-18 21:22:00 +0000

    Corrected url

--- test/Watson.DocumentConversion.Tests/RequestBuilderTests/RequestBuilderBaseTests.cs
+++ test/Watson.DocumentConversion.Tests/RequestBuilderTests/RequestBuilderBaseTests.cs
@@ -10,7 +10,7 @@ namespace Watson.DocumentConversion.Tests.RequestBuilderTests
 {
     public class RequestBuilderBaseTests
     {
-        private const string ServiceUrl = "https://gateway.watsonplatform.net/personality-insights/api/v2/profile";
+        private const string ServiceUrl = "https://gateway.watsonplatform.net/document-conversion/api/v1/convert_document?version=2015-12-15";
 
         public static IEnumerable<object[]> CreateRequestNullData => new[]
         {
